0b1dd54ce8f7647672331faa4d0bc017aa64dcec
dunbby <yoyordanov94@gmail.com>
2023-01-25 15:42:38 +0200

    form submission

--- dist/js/contact-form.js
+++ dist/js/contact-form.js
@@ -1,3 +1,4 @@
+// Handle Form Submission via Netlify
 const handleSubmit = (event) => {
   event.preventDefault();
 
@@ -9,11 +10,32 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => alert("Thank you for your submission"))
-    .catch((error) => alert(error));
+    .then(() => showAlert())
+    .catch((error) => showAlert(error));
 };
 
 document
   .querySelector("form")
   .addEventListener("submit", handleSubmit);
 
+// Submit Button Interaction
+function showAlert(response) {
+  const btn = document.querySelector(".contact-btn");
+  const sendBtn = document.querySelector(".send-btn");
+  const alertSuccess = document.querySelector(".alert-success");
+  const alertError = document.querySelector(".alert-error");
+
+  sendBtn.classList.add("hidden");
+  btn.classList.add("button-loading");
+
+  setTimeout(() => {
+    btn.classList.remove("button-loading");
+
+    if(response) {
+      alertError.classList.remove("hidden");
+    } 
+    else {
+      alertSuccess.classList.remove("hidden");
+    }
+  }, 2200);
+}
